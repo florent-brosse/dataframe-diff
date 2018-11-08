54d654e34a3a849778b5d6f2fd6e55cf96325bcf
Florent Brosse <florent.brosse@datastax.com>
2018-11-08 17:24:58 +0100

    add Map and Binary support

--- src/main/scala/io/github/diff/DiffDataframe.scala
+++ src/main/scala/io/github/diff/DiffDataframe.scala
@@ -1,25 +1,51 @@
 package io.github.diff
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 
 object DiffDataframe {
   /**
     *
     * df1 is the previous dataframe
     * df2 is the new dataframe
-    * pks is the seq of the primary keys
+    * pks is the set of the primary keys
     *
     * If df1 and df2 have a column name with a dot it will be replace by a underscore
+    * equality on MapStruct does not work with Dataframe so this type is compared separately
     *
     */
   def diff(df1: DataFrame, df2: DataFrame, pks: Set[String]) = {
+    diffWithIgnoredColumns(df1, df2, pks, Set())
+  }
+
+  /**
+    *
+    * df1 is the previous dataframe
+    * df2 is the new dataframe
+    * pks is the set of the primary keys
+    * ignoreColumns is the set of ignore column during the compare
+    *
+    * If df1 and df2 have a column name with a dot it will be replace by a underscore
+    * equality on MapStruct does not work with Dataframe so this type is compared separately
+    *
+    */
+  def diffWithIgnoredColumns(df1: DataFrame, df2: DataFrame, pks: Set[String], ignoreColumns: Set[String]) = {
     val dfOld = df1.toDF(df1.columns.map(_.replaceAll("\\.|\\s+", "_").toLowerCase): _*).alias("old")
 
     val dfNew = df2.toDF(df2.columns.map(_.replaceAll("\\.|\\s+", "_").toLowerCase): _*).alias("new")
 
+
+    assert(!pks.isEmpty)
     assert(dfOld.columns sameElements dfNew.columns)
     assert(pks subsetOf dfOld.columns.toSet)
+
+    val pksSeq = pks.toSeq
+
+    val mapColumns = dfNew.schema.filter(structIsMapOrContainsMap(_)).map(_.name).toSet diff ignoreColumns
+
     val otherColumns = dfOld.columns.toSet diff pks
+    assert(ignoreColumns subsetOf otherColumns)
+    val otherColumnsWithoutIgnored = otherColumns diff ignoreColumns
 
     val columnPk = pks.map(colName => dfOld("old." + colName) === dfNew("new." + colName))
     val joinPkCol = columnPk.reduce((a, b) => a.and(b))
@@ -32,15 +58,53 @@ object DiffDataframe {
     val columnPkIsNullNew = pks.map(colName => cached("new." + colName) isNull)
     val pkColIsNullNew = columnPkIsNullNew.reduce((a, b) => a.and(b))
 
-    val otherColumnsIsNotEquals = otherColumns.map(colName => cached("new." + colName) notEqual (cached("old." + colName)))
-    val otherColumnsIsNotEqualsCol = otherColumnsIsNotEquals.reduce((a, b) => a.or(b))
 
     val newRows = cached.filter(pkColIsNullOld).select("new.*")
 
-    val deleteRows = cached.filter(pkColIsNullNew).select("old.*").select(pks.head, pks.toSeq.tail: _*)
+    val deleteRows = cached.filter(pkColIsNullNew).select("old.*").select(pksSeq.head, pksSeq.tail: _*)
 
-    val updateRows = cached.filter(joinPkCol and otherColumnsIsNotEqualsCol).select("new.*")
-    (newRows, deleteRows, updateRows)
+    val samePkRows = cached.filter(joinPkCol)
+    val updateRows: Dataset[Row] = if(mapColumns.isEmpty){
+       if (!otherColumnsWithoutIgnored.isEmpty) {
+        val otherColumnsIsNotEquals = otherColumnsWithoutIgnored.map(colName => cached("new." + colName) notEqual (cached("old." + colName)))
+        val otherColumnsIsNotEqualsCol = otherColumnsIsNotEquals.reduce((a, b) => a.or(b))
+        samePkRows.filter(otherColumnsIsNotEqualsCol)
+      } else {
+        samePkRows.filter(row => false) //empty DF
+      }
+    }else{
+      samePkRows.filter(filterCustom(_)(otherColumnsWithoutIgnored))
+    }
+    val updateRowsWithOnlyNeedColumns = updateRows.select("new.*")
+    (newRows, deleteRows, updateRowsWithOnlyNeedColumns)
+  }
+
+  def structIsMapOrContainsMap(structField: StructField): Boolean = {
+    structField.dataType.typeName match {
+      case "map" => true
+      case "struct" => structField.dataType.asInstanceOf[StructType].exists(structIsMapOrContainsMap(_))
+      case _ => false
+    }
+  }
+
+  def filterCustom (row :Row)(otherColumnsWithoutIgnored :Set[String]): Boolean ={
+    val fieldNames = row.schema.fieldNames
+    for (columnName <- otherColumnsWithoutIgnored) {
+      val firstIndexOfColumn = fieldNames.indexOf(columnName)
+      val lastIndexOfColumn = fieldNames.indexOf(columnName,firstIndexOfColumn+1)
+      val data = row.get(firstIndexOfColumn)
+      val data2 = row.get(lastIndexOfColumn)
+      if(data != data2){
+        if((data.isInstanceOf[Array[Byte]]) && (data2.isInstanceOf[Array[Byte]]) ){
+          if(data.asInstanceOf[Array[Byte]].deep != data2.asInstanceOf[Array[Byte]].deep){
+            return true
+          }
+        }else{
+          return true
+        }
+      }
+    }
+    false
   }
 
 }
--- src/test/scala/io/github/diff/DiffDataframeTest.scala
+++ src/test/scala/io/github/diff/DiffDataframeTest.scala
@@ -1,9 +1,14 @@
 package io.github.diff
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
+
+case class Data(num :Int, data :String)
+
+case class Data2(num :Int, map :Map[String,String])
 
 
 @RunWith(classOf[JUnitRunner])
@@ -13,7 +18,7 @@ class DiffDataframeTest extends FunSuite {
 
     val spark = SparkSession.builder()
       .master("local[*]")
-      .config("spark.ui.enabled","false")
+      .config("spark.ui.enabled", "false")
       .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
@@ -36,13 +41,223 @@ class DiffDataframeTest extends FunSuite {
 
     val (newRows, deleteRows, updateRows) = DiffDataframe.diff(values.toDF("id", "id2", "w.ord", "d ata"), values2.toDF("id", "id2", "w.ord", "d ata"), pks)
 
-    val newRowsExpected= Seq((63, 1, "mouse", "2")).toDF("id", "id2", "w_ord", "d_ata")
+    val newRowsExpected = Seq((63, 1, "mouse", "2")).toDF("id", "id2", "w_ord", "d_ata")
     assert(newRowsExpected.collect() sameElements newRows.collect())
 
-    val deleteRowsExpected= Seq((64, 1)).toDF("id", "id2")
+    val deleteRowsExpected = Seq((64, 1)).toDF("id", "id2")
     assert(deleteRowsExpected.collect() sameElements deleteRows.collect())
 
-    val updateRowsExpected= Seq((8, 1, "bat", "1")).toDF("id", "id2", "w_ord", "d_ata")
+    val updateRowsExpected = Seq((8, 1, "bat", "1")).toDF("id", "id2", "w_ord", "d_ata")
     assert(updateRowsExpected.collect() sameElements updateRows.collect())
+  }
+  test("test with ignored columns") {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .config("spark.ui.enabled", "false")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val pks = Set("id")
+
+    val ignoredColumns = Set("id2")
+
+    val values = Seq(
+      (8, 1, "bat", "2"),
+      (64, 1, "mouse", "2"),
+      (1, 1, "mouse", "2"),
+      (-27, 1, "horse", "3")
+    )
+
+    val values2 = Seq(
+      (8, 1, "bat", "1"),
+      (63, 1, "mouse", "2"),
+      (1, 2, "mouse", "2"),
+      (-27, 1, "horse", "3")
+    )
+
+    val (newRows, deleteRows, updateRows) = DiffDataframe.diffWithIgnoredColumns(values.toDF("id", "id2", "w.ord", "d ata"), values2.toDF("id", "id2", "w.ord", "d ata"), pks, ignoredColumns)
+
+    val newRowsExpected = Seq((63, 1, "mouse", "2")).toDF("id", "id2", "w_ord", "d_ata")
+    assert(newRowsExpected.collect() sameElements newRows.collect())
+
+    val deleteRowsExpected = Seq((64)).toDF("id")
+    assert(deleteRowsExpected.collect() sameElements deleteRows.collect())
+
+    val updateRowsExpected = Seq((8, 1, "bat", "1")).toDF("id", "id2", "w_ord", "d_ata")
+    assert(updateRowsExpected.collect() sameElements updateRows.collect())
+  }
+
+  test("test with all types") {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .config("spark.ui.enabled", "false")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val pks = Set("_1")
+
+    val byte: Byte = 1
+    val short: Short = 1
+    val int: Int = 1
+    val long: Long = 1
+    val float: Float = 1
+    val double: Double = 1
+
+
+    val values = Seq(
+      (8, byte, short, int, long, float, double, new java.math.BigDecimal(1), "test", Array[String]("192", "168"),Array[Byte](192.toByte, 168.toByte, 1, 9)
+        , false, new java.sql.Timestamp(1), new java.sql.Date(1), Seq("tre", "tr"), Map("test" -> "value"), Data(1, "hello"))
+    )
+
+    val values2 = Seq(
+      (8, byte, short, int, long, float, double, new java.math.BigDecimal(1), "test", Array[String]("192", "168"),Array[Byte](192.toByte, 168.toByte, 1, 8)
+        , false, new java.sql.Timestamp(1), new java.sql.Date(1), Seq("tre", "tr"), Map("test" -> "value"), Data(1, "hello"))
+    )
+
+    val valuesDF = values.toDF()
+    val values2DF = values2.toDF()
+
+    val (newRows, deleteRows, updateRows) = DiffDataframe.diff(values.toDF(),values.toDF(), pks)
+    assert(newRows.count() === 0)
+    assert(deleteRows.count() === 0)
+    assert(updateRows.count() === 0)
+
+    val (newRows2, deleteRows2, updateRows2) = DiffDataframe.diff(valuesDF,values2DF, pks)
+    assert(newRows2.count() === 0)
+    assert(deleteRows2.count() === 0)
+    assert(updateRows2.count() === 1)
+
+    val (newRows3, deleteRows3, updateRows3) = DiffDataframe.diffWithIgnoredColumns(valuesDF,values2DF, pks,Set("_16"))
+    assert(newRows3.count() === 0)
+    assert(deleteRows3.count() === 0)
+    assert(updateRows3.count() === 1)
+  }
+
+  test("test with Map") {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .config("spark.ui.enabled", "false")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val pks = Set("_1")
+
+    val values = Seq(
+      (8, Map("test" -> "value"),Map("test" -> "value"), Data(1, "hello"))
+    )
+
+    val values2 = Seq(
+      (8, Map("test" -> "value"),Map("test" -> "value2"), Data(1, "hello"))
+    )
+
+    val valuesDF = values.toDF()
+    val values2DF = values2.toDF()
+
+    val (newRows, deleteRows, updateRows) = DiffDataframe.diff(valuesDF,values2DF, pks)
+    assert(newRows.count() === 0)
+    assert(deleteRows.count() === 0)
+    assert(updateRows.count() === 1)
+
+    val values3 = Seq(
+      (8, Map("test" -> "value"),Map("test" -> "value"), Data(1, "hello"))
+    )
+
+    val values4 = Seq(
+      (8, Map("test" -> "value2"),Map("test" -> "value2"), Data(1, "hello"))
+    )
+
+    val values3DF = values3.toDF()
+    val values4DF = values4.toDF()
+
+    val (newRows2, deleteRows2, updateRows2) = DiffDataframe.diff(values3DF,values4DF, pks)
+    assert(newRows2.count() === 0)
+    assert(deleteRows2.count() === 0)
+    assert(updateRows2.count() === 1)
+
+    val (newRows3, deleteRows3, updateRows3) = DiffDataframe.diff(values4.toDF(),values4.toDF(), pks)
+    assert(newRows3.count() === 0)
+    assert(deleteRows3.count() === 0)
+    assert(updateRows3.count() === 0)
+  }
+
+  test("test with nested Map") {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .config("spark.ui.enabled", "false")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val pks = Set("_1")
+
+    val values = Seq(
+      (8, Map("test" -> "value"),Map("test" -> "value"), Data2(1, Map("hello"-> "data")))
+    )
+
+    val values2 = Seq(
+      (8, Map("test" -> "value"),Map("test" -> "value"), Data2(1, Map("hello"-> "data2")))
+    )
+
+    val valuesDF = values.toDF()
+    val values2DF = values2.toDF()
+
+    val (newRows, deleteRows, updateRows) = DiffDataframe.diff(valuesDF,values2DF, pks)
+    assert(newRows.count() === 0)
+    assert(deleteRows.count() === 0)
+    assert(updateRows.count() === 1)
+
+    val (newRows2, deleteRows2, updateRows2) = DiffDataframe.diff(valuesDF,valuesDF, pks)
+    assert(newRows2.count() === 0)
+    assert(deleteRows2.count() === 0)
+    assert(updateRows2.count() === 0)
+  }
+  test("test without data") {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .config("spark.ui.enabled", "false")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val pks = Set("id")
+
+    val values = Seq(
+      (8)
+    )
+
+    val values2 = Seq(
+      (7)
+    )
+
+    val valuesDF = values.toDF("id")
+    val values2DF = values2.toDF("id")
+
+    val (newRows, deleteRows, updateRows) = DiffDataframe.diff(valuesDF,values2DF, pks)
+    assert(newRows.count() === 1)
+    assert(deleteRows.count() === 1)
+    assert(updateRows.count() === 0)
+
+    val (newRows2, deleteRows2, updateRows2) = DiffDataframe.diff(valuesDF,valuesDF, pks)
+    assert(newRows2.count() === 0)
+    assert(deleteRows2.count() === 0)
+    assert(updateRows2.count() === 0)
   }
 }
